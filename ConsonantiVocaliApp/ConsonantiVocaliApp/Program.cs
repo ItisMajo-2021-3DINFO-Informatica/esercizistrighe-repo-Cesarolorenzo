c754632b84f0f5fd40c4bce3cd3f48a116257d08
Cesarolorenzo <98150960+Cesarolorenzo@users.noreply.github.com>
2022-02-04 10:45:57 +0100

    inizio primo es

---
+++ ConsonantiVocaliApp/ConsonantiVocaliApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsonantiVocaliApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string parola;
+            int N;
+            int i = 0;
+                int contavocale = 0;
+            Console.WriteLine("inserisci numero di parole da inserire");
+            N = Convert.ToInt32(Console.ReadLine());
+
+
+            while(i<=N)
+            {
+                Console.WriteLine("inserisci parola");
+                parola = Convert.ToInt32(Console.ReadLine());
+                char carattere = parola[0];
+
+                if
+            }
